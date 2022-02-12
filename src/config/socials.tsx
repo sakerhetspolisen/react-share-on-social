4d66d7b8d4a1730f21e0a5a962dce80202a1dde4
Karl F. Sellergren <68159964+sakerhetspolisen@users.noreply.github.com>
2022-02-12 17:58:10 +0100

    Changed shareTo email value from "mail" to "email"

--- src/config/socials.tsx
+++ src/config/socials.tsx
@@ -68,7 +68,7 @@ export const iconList: IconListType = {
     color: "#c8232c",
     url: (l) => `http://pinterest.com/pin/create/link/?url=${l}`,
   },
-  mail: {
+  email: {
     title: "Email",
     path: (
       <path d="M20 4H4a2 2 0 00-2 2v12c0 1.1.9 2 2 2h16a2 2 0 002-2V6a2 2 0 00-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z" />
